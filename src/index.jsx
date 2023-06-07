23ae47a7d231fa613cfff2cf81ac2eb3640679c1
Aaron Lorenzo Woods <83775388+guitarbeat@users.noreply.github.com>
2023-06-07 23:17:53 +0000

    update stuff

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 import WebGLCanvas from './WebGLCanvas';
 
 function App() {
-  return <WebGLCanvas />;
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <WebGLCanvas />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
